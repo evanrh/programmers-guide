c43c39e9266a41514dfad2df9d0577c5cd4133bb
Evan Hastings <evanrh@disroot.org>
2021-05-26 15:58:25 -0400

    Updated build script to count words, and added more content to book

--- build.sh
+++ build.sh
@@ -1,10 +1,47 @@
 #!/bin/bash
+
 LOC=buildfiles
 OUTDIR=output
+INPUT=guide.tex
+
+function Help() {
+   echo "Build LaTeX file into a pdf or count words"
+   echo
+   echo "Syntax: build.sh [-h] [count]"
+   echo
+   echo "options:"
+   echo "count    Counts the words in guide"
+   echo
+   echo "Passing no arguments will build the project into a pdf"
+}
+
+function build() {
+   run "pdflatex -output-directory /$OUTDIR $LOC/$INPUT"
+}
+
+function count() {
+   run "texcount $LOC/$INPUT"
+}
+
+function run() {
+   docker run \
+      -v $PWD/src/:/$LOC \
+      -v $PWD/$OUTDIR:/$OUTDIR \
+      -u $USERID:$GROUPID \
+      texlive/texlive \
+      $1
+}
+
+while getopts ":h" option; do
+   case $option in
+      h)
+         Help
+         exit;;
+   esac
+done
 
 # Create output directory
-if [[ ! -d ./$OUTDIR ]]
-then
+if [[ ! -d ./$OUTDIR ]]; then
    echo "Creating output directory"
    mkdir -p ./$OUTDIR
 fi
@@ -12,9 +49,10 @@ fi
 USERID=$(stat -c '%u' $OUTDIR)
 GROUPID=$(stat -c '%g' $OUTDIR)
 
-docker run \
-   -v $PWD/src/:/$LOC \
-   -v $PWD/$OUTDIR:/$OUTDIR \
-   -u $USERID:$GROUPID \
-   texlive/texlive \
-   pdflatex -output-directory /$OUTDIR $LOC/guide.tex 
+if [[ "$#" -eq 0 ]]; then
+   build
+else
+   if [[ $1 -eq "count" ]]; then
+      count
+   fi
+fi
